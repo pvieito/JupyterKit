f6a79081a3213874488b67c8b50d54a3cb2ea557
Pedro José Pereira Vieito <pvieito@gmail.com>
2020-04-10 20:49:48 +0200

    [JupyterKit] Updated tests

--- Tests/JupyterKitTests/JupyterManagerTests.swift
+++ Tests/JupyterKitTests/JupyterManagerTests.swift
@@ -13,46 +13,45 @@ import XCTest
 
 class TabulaKitTests: XCTestCase {
     func testJupyterManager() throws {
-        if let pythonProcess = try? JupyterManager.getPythonProcess() {
-            pythonProcess.arguments = ["-m", "notebook", "list", "--json"]
-            if let _ = try? pythonProcess.runAndGetOutputString() {
-                do {
-                    try JupyterManager.stopNotebookServers()
-                    try JupyterManager.launchNotebookServer()
-                    let l1 = try JupyterManager.listNotebookServers()
-                    
-                    XCTAssertEqual(l1.count, 1)
-                    
-                    try JupyterManager.launchNotebookServer()
-                    try JupyterManager.launchNotebookServer()
-                    let l2 = try JupyterManager.listNotebookServers()
-                    
-                    XCTAssertEqual(l2.count, 3)
-                    
-                    let l2s = l2.last!
-                    try l2s.stop()
-                    let l3 = try JupyterManager.listNotebookServers()
-                    
-                    let l2Set = Set(l2[0...1].map({ $0.identifier }))
-                    let l3Set = Set(l3.map({ $0.identifier }))
-
-                    XCTAssertEqual(l3.count, 2)
-                    XCTAssertEqual(l3Set, l2Set)
-                    XCTAssertTrue(l3Set.intersection(Set([l2s.identifier])).isEmpty)
-
-                    try JupyterManager.stopNotebookServers()
-                    let l4 = try JupyterManager.listNotebookServers()
-                    
-                    XCTAssertEqual(l4.count, 0)
-                    return
-                }
-                catch {
-                    try? JupyterManager.stopNotebookServers()
-                    throw error
-                }
-            }
+        do {
+            try JupyterManager.stopNotebookServers()
+            try JupyterManager.launchNotebookServer()
+            let l1 = try JupyterManager.listNotebookServers()
+            
+            XCTAssertEqual(l1.count, 1)
+            
+            try JupyterManager.launchNotebookServer()
+            try JupyterManager.launchNotebookServer()
+            let l2 = try JupyterManager.listNotebookServers()
+            
+            XCTAssertEqual(l2.count, 3)
+            
+            let l2s = l2.last!
+            try l2s.stop()
+            
+            Thread.sleep(forTimeInterval: 1.0)
+            
+            let l3 = try JupyterManager.listNotebookServers()
+            
+            let l2Set = Set(l2[0...1].map({ $0.identifier }))
+            let l3Set = Set(l3.map({ $0.identifier }))
+            
+            XCTAssertEqual(l3.count, 2)
+            XCTAssertEqual(l3Set, l2Set)
+            XCTAssertTrue(l3Set.intersection(Set([l2s.identifier])).isEmpty)
+            
+            try JupyterManager.stopNotebookServers()
+            
+            Thread.sleep(forTimeInterval: 1.0)
+            
+            let l4 = try JupyterManager.listNotebookServers()
+            
+            XCTAssertEqual(l4.count, 0)
+            return
         }
-        
-        XCTAssertThrowsError(try JupyterManager.listNotebookServers())
+        catch {
+            try? JupyterManager.stopNotebookServers()
+            throw error
+        }
     }
 }
